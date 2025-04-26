da533be2dd64fa5136c632111d2ef7e17950435b
ST10485005 <ST10485005@vcconnect.edu.za>
2025-04-26 09:56:49 +0200

    This is the initial got commit

---
+++ app/src/main/java/com/example/linking_activities/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.linking_activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val buttonClick = findViewById<Button>(R.id.button_click)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
